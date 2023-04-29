6485856750af4c1784332f2eb038b8489e4c9de2
Ricardo Alencar <ricfilho0007@gmail.com>
2023-04-28 22:26:15 -0300

    criação e estilização da tela de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,40 @@
+import './signin.css'
+
+import { useState } from 'react'
+
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
+
 export default function SignIn(){
+
+    const [email, setEmail] = useState('');
+    const [password, setpassword] = useState('');
+
     return(
-        <div>
-            <h1>Página de Login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='logo' />
+                </div>
+
+                <form>
+                    <h1>Fazer Login</h1>
+                    <input type='text'
+                    placeholder='email@email.com'
+                    value={email}
+                    onChange={ (e) => setEmail(e.target.value) }/>
+
+                    <input type='password'
+                    placeholder='******'
+                    value={password}
+                    onChange={ (e) => setpassword(e.target.value) }/>
+                
+                    <button type='submit'>Acessar</button>
+                </form>
+                
+                <Link to='/register'>Criar uma conta</Link>
+
+            </div>
         </div>
     )
 }
