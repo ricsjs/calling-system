d04da74b6747e03b964358328916f3f817f8e969
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-04 13:10:54 -0300

    correção firebaseConnection

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,126 +1,80 @@
-import { useContext, useState } from 'react';
-import Header from '../../components/Header';
-import Title from '../../components/Title';
-import { FiSettings, FiUpload } from 'react-icons/fi';
-import avatar from '../../assets/avatar.png';
-import { AuthContext } from '../../contexts/auth';
-import { db, storage } from '../../services/firebaseConnection';
-import { doc, updateDoc } from 'firebase/firestore';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { toast } from 'react-toastify';
-import './profile.css';
+import { useContext, useState } from 'react'
+import Header from '../../components/Header'
+import Title from '../../components/Title'
 
-export default function Profile() {
+
+import { FiSettings, FiUpload } from 'react-icons/fi'
+import avatar from '../../assets/avatar.png'
+
+import { AuthContext } from '../../contexts/auth'
+
+import './profile.css'
+import { toast } from 'react-toastify'
+
+
+export default function Profile(){
+
     const { user, storageUser, setUser, logout } = useContext(AuthContext);
-    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
-    const [imageAvatar, setImageAvatar] = useState(null);
-    const [nome, setNome] = useState(user && user.nome);
-    const [email, setEmail] = useState(user && user.email);
 
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
+    const [nome, setNome] = useState(user && user.nome)
+    const [email, setEmail] = useState(user && user.email)
+    const [imageAvatar, setImageAvatar] = useState(null)
 
-    async function handleUpload() {
-        try {
-            const storageRef = ref(storage, `avatars/${user.uid}/${imageAvatar.name}`);
-            await uploadBytes(storageRef, imageAvatar);
-            const url = await getDownloadURL(storageRef);
-
-            const docRef = doc(db, 'users', user.uid);
-            await updateDoc(docRef, { nome, avatarUrl: url });
-            const updatedUser = { ...user, nome, avatarUrl: url };
-            setUser(updatedUser);
-            storageUser(updatedUser);
-            toast.success('Atualizado com sucesso!');
-        } catch (error) {
-            console.error(error);
-            toast.error('Erro ao atualizar o usuário');
-        }
-    }
-
-
-    function handleFile(e) {
-        if (e.target.files[0]) {
+    function handleFile(e){
+        if(e.target.files[0]){
             const image = e.target.files[0];
-            if (image.type === 'image/jpeg' || image.type === 'image/png') {
-                setImageAvatar(image);
-                setAvatarUrl(URL.createObjectURL(image));
-            } else {
-                toast.error('Envie uma imagem do tipo PNG ou JPEG');
-                setImageAvatar(null);
-                return null;
+
+            if(image.type === 'image/jpeg' || image.type === 'image/png'){
+                setImageAvatar(image)
+                setAvatarUrl(URL.createObjectURL(image))
+            }else{
+                toast.error("Envie uma imagem do tipo PNG ou JPEG")
             }
         }
     }
 
-
-
-    async function handleSubmit(e) {
-        e.preventDefault();
-        try {
-            if (nome === '') {
-                throw new Error('Por favor, informe um nome');
-            }
-
-            if (imageAvatar === null) {
-                const docRef = doc(db, 'users', user.uid);
-                await updateDoc(docRef, { nome });
-                const updatedUser = { ...user, nome };
-                setUser(updatedUser);
-                storageUser(updatedUser);
-                toast.success('Atualizado com sucesso!');
-            } else {
-                //implementar a função handleUpload() para fazer upload de imagem no firebase
-                handleUpload();
-            }
-        } catch (error) {
-            console.error(error);
-            toast.error('Erro ao atualizar o usuário');
-        }
-    }
-
-
-    return (
+    return(
         <div>
-            <Header />
-
-            <div className="content">
-                <Title name="Minha conta">
-                    <FiSettings size={25} />
+            <Header/>
+            <div className='content'>
+                <Title name="Meu perfil">
+                    <FiSettings size={25}/>
                 </Title>
+                
+                <div className='container'>
+                    
+                    <form className='form-profile'>
 
-                <div className="container">
-
-                    <form className="form-profile" onSubmit={handleSubmit}>
-                        <label className="label-avatar">
+                        <label className='label-avatar'>
                             <span>
-                                <FiUpload color="#FFF" size={25} />
+                                <FiUpload color='#FFF' size={25}/>
                             </span>
 
-                            <input type="file" accept="image/*" onChange={handleFile} /> <br />
-                            {avatarUrl === null ? (
-                                <img src={avatar} alt="Foto de perfil" width={250} height={250} />
+                            <input type='file' accept='image/*' onChange={handleFile}/><br/>
+                            {avatarUrl === null ?(
+                                <img src={avatar} alt='Foto de perfil' width={250} height={250}/>
                             ) : (
-                                <img src={avatarUrl} alt="Foto de perfil" width={250} height={250} />
+                                <img src={avatarUrl} alt='Foto de perfil' width={250} height={250}/>
                             )}
-
                         </label>
 
                         <label>Nome</label>
-                        <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
+                        <input type='text' placeholder={user.nome} onChange={ (e) => setNome(e.target.value) }/>
 
-                        <label>Email</label>
-                        <input type="text" value={email} disabled={true} />
+                        <label>E-mail</label>
+                        <input type='text' value={user.email} disabled={true}/>
 
-                        <button type="submit">Salvar</button>
+                        <button type='submit'>Salvar</button>
                     </form>
 
                 </div>
 
-                <div className="container">
-                    <button className="logout-btn" onClick={() => logout()}>Sair</button>
+                <div className='container'>
+                    <button onClick={() => logout()} className='logout-btn'>Sair</button>
                 </div>
 
             </div>
-
         </div>
     )
 }
