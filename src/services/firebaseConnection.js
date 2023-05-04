12a706d67c21116aeace827ac6e9b2ffe5585ec3
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-04 13:05:27 -0300

    correção do firebaseConnection e atualizando perfil

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -17,6 +17,6 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
-const storage = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp);
 
 export { auth, db, storage };
