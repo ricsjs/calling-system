e171e1128e59f5cc6b834c6f8b70b268d1d645c4
Ricardo Alencar <ricfilho0007@gmail.com>
2023-05-04 15:23:03 -0300

    página novo chamado pt 2

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,21 +1,73 @@
 import './new.css'
 
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 
 import Title from '../../components/Title'
 import Header from "../../components/Header";
 import { FiPlusCircle } from 'react-icons/fi';
 
+import { AuthContext } from '../../contexts/auth'
+
+import { db } from '../../services/firebaseConnection'
+import { collection, getDocs, getDoc, doc } from 'firebase/firestore';
+
+const listRef = collection(db, "customers");
+
 export default function New(){
+
+    const { user } = useContext(AuthContext)
     
     const [customers, setCustomers] = useState([])
+    const [loadCustomer, setLoadCustomer] = useState(true)
+    const [customerSelected, setCustomerSelected] = useState(0)
 
     const [complemtento, setComplemento] = useState('')
     const [assunto, setAssunto] = useState('Suporte')
     const [status, setStatus] = useState('Aberto')
 
+    useEffect(() => {
+        async function loadCustomers(){
+            const querySnapshot = await getDocs(listRef)
+            .then((snapshopt) => {
+                let lista = [];
+              
+                snapshopt.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        nomeFantasia: doc.data().nomeFantasia
+                    })
+                })
+                if(snapshopt.docs.size === 0){
+                    console.log("Nenhum cliente encontrado")
+                    setCustomers([ {id: '1', nomeFantasia: 'FREELA'} ])
+                    setLoadCustomer(false)
+                    return;
+                }
+
+                setCustomers(lista);
+                setLoadCustomer(false);
+
+            })
+            .catch((error) => {
+                console.log("Erro ao buscar os clientes", error)
+                setLoadCustomer(false)
+                setCustomers([ {id: '1', nomeFantasia: 'FREELA'} ])
+            })
+        }
+
+        loadCustomers();
+    }, [])
+
     function handleOptionChange(e){
         setStatus(e.target.value)
+    }
+
+    function handleChangeSelect(e){
+        setAssunto(e.target.value)
+    }
+
+    function handlChangeCustomer(e){
+        setCustomerSelected(e.target.value)
     }
 
     return(
@@ -32,14 +84,25 @@ export default function New(){
                     <form className='form-profile'>
 
                         <label>Clientes</label>
-                        <select>
-                            <option key={1} value={1}>Mercado</option>
-                            <option key={2} value={2}>Loja</option>
-                        </select>
+                        {
+                            loadCustomer ? (
+                                <input type='text' disabled={true}
+                                value="Carregando..."/>
+                            ) : (
+                                <select value={customerSelected} onChange={handlChangeCustomer}>
+                                    {customers?.map((item, index) => {
+                                        return(
+                                            <option key={index} value={index}>
+                                                {item.nomeFantasia}
+                                            </option>
+                                        )
+                                    })}
+                                </select>
+                            )
+                        }
 
                         <label>Assunto</label>
-                        <select>
-                            <option selected>Selecione</option>
+                        <select value={assunto} onChange={handleChangeSelect}>
                             <option key={1} value='Suporte'>Suporte</option>
                             <option key={2} value='Visita técnica'>Visita Ténica</option>
                             <option key={3} value='Financeiro'>Financeiro</option>
